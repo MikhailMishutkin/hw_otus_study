be99345d015bd43c792d968abca4397ae4f8edfb
Mishutkin M <mm8472274@yandex.ru>
2021-10-09 19:18:34 +0300

    Hw2 try to finish

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,36 +11,45 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	var result, result1, result2 strings.Builder // инициализирую переменную встроенного типа билдер для хранения результатов распаковки(в том числе промежуточных)
+	// инициализирую переменную встроенного типа
+	// билдер для хранения результатов распаковки(в том числе промежуточных)
+	var result, resultBefore, resultBeforeLast strings.Builder
 	var d rune
 	for i, r := range s { // проходим по строке
-		var res string  // внутрицикловая переменная для построения результирующей строки
-		k := rune(s[i]) // приводим к рунам для преобразований
-		l := rune(s[0]) // первый элемент, который может вызвать ошибку
+		// внутрицикловая переменная для построения результирующей строки
+		var res string
+		// приводим к рунам для преобразований
+		k := rune(s[i])
+		// первый элемент, который может вызвать ошибку
+		l := rune(s[0])
 		switch {
 		case unicode.IsDigit(l):
 			return "Неудача, ссорян", ErrInvalidString
 		case unicode.IsDigit(k) && unicode.IsDigit(d):
 			return "Неудача, ссорян", ErrInvalidString
 		case unicode.IsDigit(k) && k == '0':
-			result = result2
+			result = resultBeforeLast
 		case unicode.IsDigit(k) && k == '1':
-			result = result1
+			result = resultBefore
 		case unicode.IsDigit(k):
 			dig, err := strconv.Atoi(string(s[i]))
 			if err != nil {
 				log.Fatal()
 			}
-			result = result2                     // присваиваем предпредыдущий результат
-			res = strings.Repeat(string(d), dig) // повторяем предыдущее буквенное значение dig раз, записываем в промежуточную переменную
+			// присваиваем предпредыдущий результат
+			result = resultBeforeLast
+			// повторяем предыдущее буквенное
+			// значение dig раз, записываем в промежуточную переменную
+			res = strings.Repeat(string(d), dig)
 		default:
-			res = string(r) // записываем букву в промежуточную переменную
-			result2 = result1
+			// записываем букву в промежуточную переменную
+			res = string(r)
+			resultBeforeLast = resultBefore
 		}
 		d = k
 		result.WriteString(res)
-		result2 = result1
-		result1 = result
+		resultBeforeLast = resultBefore
+		resultBefore = result
 	}
 	return result.String(), nil
 }
