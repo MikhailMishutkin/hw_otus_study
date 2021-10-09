5e2d4dfde29a73c9b771acbf0dca4dcce3019e69
Mishutkin M <mm8472274@yandex.ru>
2021-10-09 17:31:07 +0300

    Hw2 try to finish

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,9 +11,9 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	var result, res1, res2 strings.Builder // инициализирую переменную встроенного типа билдер для хранения результатов распаковки(в том числе промежуточных)
+	var result, result1, result2 strings.Builder // инициализирую переменную встроенного типа билдер для хранения результатов распаковки(в том числе промежуточных)
 	var d rune
-	for i, r := range s { //проходим по строке
+	for i, r := range s { // проходим по строке
 		var res string  // внутрицикловая переменная для построения результирующей строки
 		k := rune(s[i]) // приводим к рунам для преобразований
 		l := rune(s[0]) // первый элемент, который может вызвать ошибку
@@ -23,49 +23,24 @@ func Unpack(s string) (string, error) {
 		case unicode.IsDigit(k) && unicode.IsDigit(d):
 			return "Неудача, ссорян", ErrInvalidString
 		case unicode.IsDigit(k) && k == '0':
-			result = res2
+			result = result2
 		case unicode.IsDigit(k) && k == '1':
-			result = res1
+			result = result1
 		case unicode.IsDigit(k):
 			dig, err := strconv.Atoi(string(s[i]))
 			if err != nil {
 				log.Fatal()
 			}
-			result = res2                        // присваиваем предпредыдущий результат
+			result = result2                     // присваиваем предпредыдущий результат
 			res = strings.Repeat(string(d), dig) // повторяем предыдущее буквенное значение dig раз, записываем в промежуточную переменную
 		default:
 			res = string(r) // записываем букву в промежуточную переменную
-			res2 = res1
+			result2 = result1
 		}
 		d = k
 		result.WriteString(res)
-		res2 = res1
-		res1 = result
+		result2 = result1
+		result1 = result
 	}
 	return result.String(), nil
 }
-
-/*
-
-
-	if unicode.IsDigit(l) {
-		log.Fatal(ErrInvalidString)
-	} else if unicode.IsDigit(k) { // проверяем значение цифра или нет
-		switch unicode.IsDigit(k) {
-		case condition:
-
-		}
-		if unicode.IsDigit(k) = 0 {
-
-		}
-		dig, err := strconv.Atoi(string(s[i]))
-		if err != nil {
-			log.Fatal()
-		}
-		res = strings.Repeat(string(d), dig-1) // повторяем предыдущее буквенное значение dig-1 раз, записываем в промежуточную переменную
-	} else {
-		res = string(r) // записываем букву в промежуточную переменную
-	}
-	if unicode.IsDigit(k) && unicode.IsDigit(d) {
-		log.Fatal(ErrInvalidString)
-	} */
